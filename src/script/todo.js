b5849afe93255a033f0ba07eff65a45d04c15ff7
Rudolf Schlenger <schlengerrudolfde@gmail.com>
2026-01-23 14:38:41 +0100

    Add todo module

---
+++ src/script/todo.js
@@ -0,0 +1,34 @@
+export class Todo {
+    #title;
+    #description;
+    #dueDate;
+    #priority;
+    #completionStatus;
+    #id;
+
+    constructor(title, description, dueDate, priority) {
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#id = crypto.randomUUID();
+        this.#completionStatus = false;
+    }
+
+    get getTitle() { return this.#title };
+    set setTitle(name) { this.#title = name };
+
+    get getDescription() { return this.#description };
+    set setDescription(text) { this.#description = text };
+
+    get getDueDate() { return this.#dueDate };
+    set setDueDate(date) { this.#dueDate = date };
+
+    get getPriority() { return this.#priority };
+    set setPriority(number) { this.#priority = number };
+
+    get getCompletionStatus() { return this.#completionStatus };
+    toggleCompletionStatus() { this.#completionStatus = this.#completionStatus === false ? true : false };
+
+    get getId() { return this.#id };
+}
